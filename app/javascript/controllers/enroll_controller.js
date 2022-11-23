662d8a96e03d872ffd301cfe7e6c10717aa2941d
benoit moret <benoit.moret@gmail.com>
2022-11-23 16:03:30 +0100

    Add bottom bar & js for navigation

--- app/javascript/controllers/enroll_controller.js
+++ app/javascript/controllers/enroll_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   // static targets = ["pledge", "fridge", 'solar', 'battery', 'km', 'features']
 
-  static targets = ['formBox']
+  static targets = ['formBox','fridgenav', 'solarnav', 'batterynav', 'kmnav', 'featuresnav']
   connect() {
     // console.log("enroll controller connected")
   }
@@ -33,5 +33,46 @@ export default class extends Controller {
     const index = TAB.indexOf(id)
     this.formBoxTargets[index].classList.add('d-none')
     this.formBoxTargets[index - 1].classList.remove('d-none')
+  }
+
+  goToFridge(event) {
+    const TAB = ["fridge", 'solar', 'battery', 'km', 'features', 'results']
+    var id = ""
+    id = event.path[3].id
+    const index = TAB.indexOf(id) //find the actual formbox
+    this.formBoxTargets[index].classList.add('d-none') // hide the actual formbx
+    this.formBoxTargets[0].classList.remove('d-none') //display fridge form box
+  }
+  goToSolar(event) {
+    const TAB = ["fridge", 'solar', 'battery', 'km', 'features', 'results']
+    var id = ""
+    id = event.path[3].id
+    const index = TAB.indexOf(id) //find the actual formbox
+    this.formBoxTargets[index].classList.add('d-none') // hide the actual formbx
+    this.formBoxTargets[1].classList.remove('d-none') //display fridge form box
+  }
+  goToBattery(event) {
+    const TAB = ["fridge", 'solar', 'battery', 'km', 'features', 'results']
+    var id = ""
+    id = event.path[3].id
+    const index = TAB.indexOf(id) //find the actual formbox
+    this.formBoxTargets[index].classList.add('d-none') // hide the actual formbx
+    this.formBoxTargets[2].classList.remove('d-none') //display fridge form box
+  }
+  goToKm(event) {
+    const TAB = ["fridge", 'solar', 'battery', 'km', 'features', 'results']
+    var id = ""
+    id = event.path[3].id
+    const index = TAB.indexOf(id) //find the actual formbox
+    this.formBoxTargets[index].classList.add('d-none') // hide the actual formbx
+    this.formBoxTargets[3].classList.remove('d-none') //display fridge form box
+  }
+  goToFeatures(event) {
+    const TAB = ["fridge", 'solar', 'battery', 'km', 'features', 'results']
+    var id = ""
+    id = event.path[3].id
+    const index = TAB.indexOf(id) //find the actual formbox
+    this.formBoxTargets[index].classList.add('d-none') // hide the actual formbx
+    this.formBoxTargets[4].classList.remove('d-none') //display fridge form box
   }
 }
